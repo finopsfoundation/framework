76aafb784dd27d1ef27d74519396f485c0b9f51e
Steven Trask <steve@gosupernova.co.uk>
2021-05-08 00:03:21 +0100

    Adding off canvas and footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+// Scroll to function
+function getElementY(query) {
+  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
+}
+
+function doScrolling(element, duration) {
+  var startingY = window.pageYOffset
+  var elementY = getElementY(element)
+  // If element is close to page's bottom then window will scroll only to some position above the element.
+  var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
+  var diff = targetY - startingY - 40
+  // Easing function: easeInOutCubic
+  // From: https://gist.github.com/gre/1650294
+  var easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+  var start
+
+  if (!diff) return
+
+  // Bootstrap our animation - it will get called right before next frame shall be rendered.
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp
+    // Elapsed miliseconds since start of scrolling.
+    var time = timestamp - start
+    // Get percent of completion in range [0, 1].
+    var percent = Math.min(time / duration, 1)
+    // Apply the easing.
+    // It can cause bad-looking slow frames in browser performance tool, so be careful.
+    percent = easing(percent)
+
+    window.scrollTo(0, startingY + diff * percent)
+
+    // Proceed with animation as long as we wanted it to.
+    if (time < duration) {
+      window.requestAnimationFrame(step)
+    }
+  })
+}
+
+document.getElementById('js-training-1').addEventListener('click', doScrolling.bind(null, '#focp-certification-exam--300', 1000));
+document.getElementById('js-training-2').addEventListener('click', doScrolling.bind(null, '#self-paced-online-course---499', 1500));
+document.getElementById('js-training-3').addEventListener('click', doScrolling.bind(null, '#instructor-led-live-training--890', 2000));
+
+
+
+// Off canvas menu
+document.addEventListener('DOMContentLoaded', function() {
+
+  var Nav = new hcOffcanvasNav('#off-canvas', {
+    levelOpen: 'expand',
+    navTitle: 'FinOps Foundation',
+    disableAt: '768',
+    levelTitles: true,
+    levelTitleAsBack: true
+  });
+
+});
+
+
+
 // Accordian
 
 $(function(){
