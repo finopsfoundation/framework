e9728d215a5af4ca85323bc1765793b414c04471
Steve Trask <steve@finops.org>
2022-01-03 22:21:26 +0000

    algolia test

---
+++ netlify/functions/deploy-succeeded.js
@@ -0,0 +1,38 @@
+const algoliasearch = require('algoliasearch');
+
+const { ALGOLIA_KEY } = process.env;
+
+const contentJSON = [
+  {
+    "name": "Foo"
+  }
+];
+
+exports.handler = (event, context, callback) => {
+  // Here we'll use Algolia to get upload
+  var client = algoliasearch('64LMXTN0XN', ALGOLIA_KEY);
+	var index = client.initIndex('prod_site-content');
+	var contentJSON = require('../../_site/json/content.json');
+
+	// var contentJSON = [
+	//   {
+	//   	"objectID": "1",
+	//     "name": "Foo"
+	//   }
+	// ];
+
+	index.clearObjects().then(() => {
+  // clear done
+	  index.saveObjects(contentJSON, function(err, content) {
+		  if (err) {
+		    console.error(err);
+		    callback(err);
+		  } else {
+		  	callback(null, {
+			    statusCode: 200,
+			    body: 'Sucess for a better search!',
+			  });
+		  }
+		});
+	});
+};
