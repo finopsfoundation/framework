e0fe2ed86cef072156dbbdae586b7dd0e5ef6454
Steve Trask <steve@finops.org>
2022-01-16 20:19:55 +0000

    adding label

--- js/partials/search.js
+++ js/partials/search.js
@@ -16,6 +16,9 @@ search.addWidgets([
       item: `
         <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border rounded-lg shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
           <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
+          {{#label}}
+          <span class="text-white font-medium bg-green-500 rounded-md py-1 px-2 inline-block uppercase text-xs leading-tight">{{ label }}</span>
+          {{/label}}
           <p class="block text-gray-600 text-sm leading-relaxed mb-2"">{{ meta_desc }}</p>
           <span class="text-gray-600 hover:text-green-500 text-sm leading-relaxed underline">Read more</span>
         </a>
