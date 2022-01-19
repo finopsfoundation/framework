96c33b7196f3f657f888560619158a13db6d1d5c
Steve Trask <steve@finops.org>
2022-01-19 21:38:34 +0000

    stash

--- js/partials/search.js
+++ js/partials/search.js
@@ -1,5 +1,6 @@
 const searchClient = algoliasearch('64LMXTN0XN', '3b7af83f7cdd87204a4c8412426bd278');
 
+// Capability page
 if(document.getElementById('capability-hits')) {
 
   const capability_search = instantsearch({
@@ -33,34 +34,111 @@ if(document.getElementById('capability-hits')) {
 
 }
 
-if(document.getElementById('resources-hits')) {
+// Resources page
+if(document.getElementById('resources-hits_capability')) {
 
-  const resources_search = instantsearch({
+  const resourceIndex = instantsearch({
     indexName: 'prod_resources',
     searchClient,
   });
 
-  resources_search.addWidgets([
-    instantsearch.widgets.searchBox({
-      container: '#resources-search',
+  resourceIndex.addWidgets([
+    instantsearch.widgets.configure({
+      hitsPerPage: 6,
     }),
-
     instantsearch.widgets.hits({
-      container: '#resources-hits',
+      container: '#resources-hits_resources',
       templates: {
         item: `
-          <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border rounded-lg shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
-            <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
-            {{#label}}
-            <span class="text-white font-medium bg-green-500 rounded-md py-1 px-2 inline-block uppercase text-xs leading-tight">{{ label }}</span>
-            {{/label}}
-            <p class="block text-gray-600 text-sm leading-relaxed mb-2"">{{ meta_desc }}</p>
-            <span class="text-gray-600 hover:text-green-500 text-sm leading-relaxed underline">Read more</span>
+           <a class="m-0 bg-gray-200 border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+            <img src="https://via.placeholder.com/300x200.png" />
+            <div class="p-4">
+              <h2 class="text-base font-medium mt-0 mb-2">{{ meta_title }}</h2>
+              {{#label}}
+              <span class="text-white font-medium bg-green-500 rounded-md py-1 px-2 inline-block uppercase text-xs leading-tight">{{ label }}</span>
+              {{/label}}
+            </div>
           </a>
         `,
       },
-    })
+    }),
   ]);
 
-  resources_search.start();
+  const toolingIndex = instantsearch({
+    indexName: 'prod_tooling_services',
+    searchClient,
+  });
+
+  toolingIndex.addWidgets([
+    instantsearch.widgets.configure({
+      hitsPerPage: 3,
+    }),
+    instantsearch.widgets.hits({
+      container: '#resources-hits_tooling',
+      templates: {
+        item: `
+          <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+            <h2 class="text-lg font-medium mt-0 mb-2">{{ name }}</h2>
+          </a>
+        `,
+      },
+    }),
+  ]);
+
+  const storiesIndex = instantsearch({
+    indexName: 'prod_stories',
+    searchClient,
+  });
+
+  storiesIndex.addWidgets([
+    instantsearch.widgets.configure({
+      hitsPerPage: 3,
+    }),
+    instantsearch.widgets.hits({
+      container: '#resources-hits_stories',
+      templates: {
+        item: `
+          <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+            <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
+          </a>
+        `,
+      },
+    }),
+  ]);
+
+  const capabilityIndex = instantsearch({
+    indexName: 'prod_capabilities',
+    searchClient,
+    searchFunction(helper) {
+      storiesIndex.helper.setQuery(helper.state.query).search();
+      resourceIndex.helper.setQuery(helper.state.query).search();
+      toolingIndex.helper.setQuery(helper.state.query).search();
+      helper.search();
+    },
+  });
+
+  capabilityIndex.addWidgets([
+    instantsearch.widgets.configure({
+      hitsPerPage: 3,
+    }),
+    instantsearch.widgets.searchBox({
+      container: '#resources-search',
+    }),
+    instantsearch.widgets.hits({
+      container: '#resources-hits_capability',
+      templates: {
+        item: `
+          <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+            <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
+          </a>
+        `,
+      },
+    }),
+  ]);
+
+  toolingIndex.start();
+  resourceIndex.start();
+  capabilityIndex.start();
+  storiesIndex.start();
+
 }
--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -15,10 +15,12 @@ exports.handler = (event, context, callback) => {
 	var index_capabilities = client.initIndex('prod_capabilities');
 	var index_tools = client.initIndex('prod_tooling_services');
 	var index_resources = client.initIndex('prod_resources');
+	var index_stories = client.initIndex('prod_stories');
 	var contentJSON = require('../../_site/json/site-content.json');
 	var contentJSON_capabilities = require('../../_site/json/capabilities.json');
 	var contentJSON_tooling = require('../../_site/json/tools-services.json');
 	var contentJSON_resources = require('../../_site/json/resources.json');
+	var contentJSON_stories = require('../../_site/json/stories.json');
 
 // Test content
 	// var contentJSON = [
@@ -77,6 +79,21 @@ exports.handler = (event, context, callback) => {
 	index_resources.clearObjects().then(() => {
   // clear done
 	  index_resources.saveObjects(contentJSON_resources, function(err, content) {
+		  if (err) {
+		    console.error(err);
+		    callback(err);
+		  } else {
+		  	callback(null, {
+			    statusCode: 200,
+			    body: 'Sucess for a better search!',
+			  });
+		  }
+		});
+	});
+	// Stories
+	index_stories.clearObjects().then(() => {
+  // clear done
+	  index_stories.saveObjects(contentJSON_stories, function(err, content) {
 		  if (err) {
 		    console.error(err);
 		    callback(err);
