92c21ee0e96aeb3c1875f1eb4420c5f1e871e277
Steve Trask <steve@finops.org>
2022-01-20 15:09:14 +0000

    stash

--- js/partials/search.js
+++ js/partials/search.js
@@ -37,108 +37,97 @@ if(document.getElementById('capability-hits')) {
 // Resources page
 if(document.getElementById('resources-hits_capability')) {
 
-  const resourceIndex = instantsearch({
-    indexName: 'prod_resources',
-    searchClient,
-  });
-
-  resourceIndex.addWidgets([
-    instantsearch.widgets.configure({
-      hitsPerPage: 6,
-    }),
-    instantsearch.widgets.hits({
-      container: '#resources-hits_resources',
-      templates: {
-        item: `
-           <a class="m-0 bg-gray-200 border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
-            <img src="https://via.placeholder.com/300x200.png" />
-            <div class="p-4">
-              <h2 class="text-base font-medium mt-0 mb-2">{{ meta_title }}</h2>
-              {{#label}}
-              <span class="text-white font-medium bg-green-500 rounded-md py-1 px-2 inline-block uppercase text-xs leading-tight">{{ label }}</span>
-              {{/label}}
-            </div>
-          </a>
-        `,
-      },
-    }),
-  ]);
-
-  const toolingIndex = instantsearch({
-    indexName: 'prod_tooling_services',
-    searchClient,
-  });
-
-  toolingIndex.addWidgets([
-    instantsearch.widgets.configure({
-      hitsPerPage: 3,
-    }),
-    instantsearch.widgets.hits({
-      container: '#resources-hits_tooling',
-      templates: {
-        item: `
-          <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
-            <h2 class="text-lg font-medium mt-0 mb-2">{{ name }}</h2>
-          </a>
-        `,
-      },
-    }),
-  ]);
-
-  const storiesIndex = instantsearch({
-    indexName: 'prod_stories',
-    searchClient,
-  });
-
-  storiesIndex.addWidgets([
-    instantsearch.widgets.configure({
-      hitsPerPage: 3,
-    }),
-    instantsearch.widgets.hits({
-      container: '#resources-hits_stories',
-      templates: {
-        item: `
-          <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
-            <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
-          </a>
-        `,
-      },
-    }),
-  ]);
-
-  const capabilityIndex = instantsearch({
+  const search = instantsearch({
     indexName: 'prod_capabilities',
     searchClient,
-    searchFunction(helper) {
-      storiesIndex.helper.setQuery(helper.state.query).search();
-      resourceIndex.helper.setQuery(helper.state.query).search();
-      toolingIndex.helper.setQuery(helper.state.query).search();
-      helper.search();
-    },
   });
 
-  capabilityIndex.addWidgets([
+  search.addWidgets([
     instantsearch.widgets.configure({
       hitsPerPage: 3,
     }),
+
     instantsearch.widgets.searchBox({
       container: '#resources-search',
     }),
+
     instantsearch.widgets.hits({
       container: '#resources-hits_capability',
       templates: {
-        item: `
-          <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
-            <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
-          </a>
-        `,
+        item:
+          `
+            <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+              <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
+            </a>
+          `,
       },
     }),
+
+    instantsearch.widgets
+      .index({ indexName: 'prod_stories' })
+      .addWidgets([
+        instantsearch.widgets.configure({
+          hitsPerPage: 3,
+        }),
+
+        instantsearch.widgets.hits({
+          container: '#resources-hits_stories',
+          templates: {
+            item: `
+            <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+              <h2 class="text-lg font-medium mt-0 mb-2">{{ meta_title }}</h2>
+            </a>
+          `,
+          },
+        }),
+      ]),
+
+    instantsearch.widgets
+      .index({ indexName: 'prod_resources' })
+      .addWidgets([
+        instantsearch.widgets.configure({
+          hitsPerPage: 6,
+        }),
+
+        instantsearch.widgets.hits({
+          container: '#resources-hits_resources',
+          templates: {
+            item:  `
+             <a class="m-0 bg-gray-200 border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+              <img src="https://via.placeholder.com/300x200.png" />
+              <div class="p-4">
+                <h2 class="text-base font-medium mt-0 mb-2">{{ meta_title }}</h2>
+                {{#label}}
+                <span class="text-white font-medium bg-green-500 rounded-md py-1 px-2 inline-block uppercase text-xs leading-tight">{{ label }}</span>
+                {{/label}}
+              </div>
+            </a>
+          `,
+          },
+        }),
+      ]),
+
+      instantsearch.widgets
+      .index({ indexName: 'prod_tooling_services' })
+      .addWidgets([
+        instantsearch.widgets.configure({
+          hitsPerPage: 6,
+        }),
+
+        instantsearch.widgets.hits({
+          container: '#resources-hits_tooling',
+          templates: {
+            item:  `
+            <a class="m-0 p-6 w-full bg-white border-solid border-gray-200 border shadow-sm hover:border-green-500 cursor-pointer transition duration-200" href="{{ url }}">
+              <h2 class="text-lg font-medium mt-0 mb-2">{{ name }}</h2>
+            </a>
+          `,
+          },
+        }),
+      ]),
+
   ]);
 
-  toolingIndex.start();
-  resourceIndex.start();
-  capabilityIndex.start();
-  storiesIndex.start();
+  search.start();
 
 }
